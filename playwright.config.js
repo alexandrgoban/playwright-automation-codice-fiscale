4209f8df80b104121bbd54e0b8e648ee1806e1f8
alex-goban <your-email@example.com>
2026-04-30 21:41:44 +0200

    docs: force update report with video and screenshots

--- playwright.config.js
+++ playwright.config.js
@@ -22,11 +22,11 @@ export default defineConfig({
     baseURL: 'https://www.codicefiscaleonline.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-    
+    trace: 'on',
     // Set video and screenshot on failure for better debugging
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    screenshot: 'on',
+    video: 'on',
+
   },
 
   /* Configure projects for major browsers */
